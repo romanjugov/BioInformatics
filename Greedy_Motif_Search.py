fe484119d9f5be38702c503311edde8ab96001d3
Roman Jugov <54271580+romanjugov@users.noreply.github.com>
2021-02-26 15:31:23 +0000

    Update Greedy_Motif_Search.py

--- Greedy_Motif_Search.py
+++ Greedy_Motif_Search.py
@@ -1,31 +1,34 @@
-# Copy your Score(Motifs), Count(Motifs), Profile(Motifs), and Consensus(Motifs) functions here.
-def Count(Motifs):
-    count = {}                 # initializing the count dictionary
-    k = len(Motifs[0])         # k = lenght of the 1st Motif 
-    for symbol in "ACGT":      # list of symbols/bases
-        count[symbol] = []     # place empty list as key value pair to each base/symbol
-        for j in range(k):     # for the length of the motif
-            count[symbol].append(0)    # Add place that amount of 0s into the empty list inside count{}
-    t = len(Motifs)                    # Length of motif list 
-    for i in range(t):                 # for amount of elements in this list Motifs[i]
-        for j in range(k):             # for length of bases in each 'element' Motifs[i][j]
-            symbol = Motifs[i][j]      # Where symbol/base key matches base on Motif
-            count[symbol][j] += 1      # Add 1 to count of each position
-    return count                       # return matrix of counts e.g. {A:[1,2,0,1,2],C:[0,1,1,1,0]}
+# Algorythm to find best motifs
 
-def Consensus(Motifs):                 
-    k = len(Motifs[0])                # k = lenght of the 1st Motif
-    count = Count(Motifs)             # run Count(Motif) method above
-    consensus = ""                    # initialise empty string
-    for j in range(k):                # for the length of the motif
-        m = 0                         # initialise count at 0
-        frequentSymbol = ""           # initialise empty string
-        for symbol in "ACGT":         # for each symbol/base in list
-            if count[symbol][j] > m:  # if count of each base in Motifs[i][j] is > 0
-                m = count[symbol][j]  # make the current count of each base in Motifs[i][j] as m
-                frequentSymbol = symbol  # make that base the most frequent base
-        consensus += frequentSymbol      # add this base to consensus sequence
-    return consensus                     # return full consensus sequence
+def Count(Motifs):
+    count = {} # initializing the count dictionary
+    # your code here
+    k = len(Motifs[0])
+    for symbol in "ACGT":
+        count[symbol] = []
+        for j in range(k):
+            count[symbol].append(0)
+    t = len(Motifs)
+    for i in range(t):
+        for j in range(k):
+            symbol = Motifs[i][j]
+            count[symbol][j] += 1
+    return count
+
+def Consensus(Motifs):
+    # insert your code here
+    k = len(Motifs[0])
+    count = Count(Motifs)
+    consensus = ""
+    for j in range(k):
+        m = 0
+        frequentSymbol = ""
+        for symbol in "ACGT":
+            if count[symbol][j] > m:
+                m = count[symbol][j]
+                frequentSymbol = symbol
+        consensus += frequentSymbol
+    return consensus
 
 def Profile(Motifs):
     t = len(Motifs)
@@ -36,49 +39,56 @@ def Profile(Motifs):
             profile[i][j] = profile[i][j]/t  
     return profile
 
-def Score(Motifs):                      
-    consensus = Consensus(Motifs)        # Consensus(Motif) method above
-    count = 0                            # initialise count at 0
-    for motif in Motifs:                 # for each motif
-        for index in range(len(motif)):  # for each position in length of a motif
-            if motif[index] != consensus[index]: # if the index does not match the consensus index
-                count += 1                       # add 1
-    return count                                 # return consensus count of consensus sequence
-# Then copy your ProfileMostProbableKmer(Text, k, Profile) and Pr(Text, Profile) functions here.
-def Pr(text, profile):
-    n = len(text)
-    k = len(profile)
-    p = 1
-    for i in range(n):
-        symbol = text[i]
-        p *= profile[symbol][i]
+def Score(Motifs):
+    # Insert code here
+    score = 0
+    k = len(Motifs[0])
+    count = Count(Motifs)
+    max_symbol = Consensus(Motifs)
+    sum1 = 0
+    for j in range(k):
+        m = 0
+        for symbol in "ATCG":
+            if count[symbol][j] > m:
+                sum1 += count[symbol][j]
+    for j in range(k):
+        m = 0
+        for symbol in "AGTC":
+            if count[symbol][j] > m:
+                m = count[symbol][j]
+        score += m  
+    return sum1-score
+
+def Pr(Text, Profile):
+    p=1
+    k = len(Profile["A"])
+    for i in range(len(Text)):
+        p=p*Profile[Text[i]][i]
     return p
 
-def ProfileMostProbablePattern(text, k, profile):
-    n = len(text)
-    pr = {}
-    most_prob_kmer = []
-    for i in range(n-k+1):
-        k_mer = text[i:i+k]
-        probability = Pr(k_mer, profile)
-        pr[k_mer] = probability
-    m = max(pr.values())
-    for key, value in pr.items():
-        if pr[key] == m:
-            most_prob_kmer.append(key)
-    return most_prob_kmer[0]
 
-def GreedyMotifSearch(Dna, k, t):
+def ProfileMostProbablePattern(text,k,profile):
+    p=-1
+    result=text[0:k]
+    for i in range(len(text)-k+1):
+        seq=text[i:i+k]
+        pr=Pr(seq,profile)
+        if pr>p:
+            p=pr
+            result=seq
+    return result
+
+def GreedyMotifSearch(Dna,k,t):
     BestMotifs = []
     for i in range(0, t):
         BestMotifs.append(Dna[i][0:k])
     n = len(Dna[0])
-    for i in range(n-k+1):
+    for m in range(n-k+1):
         Motifs = []
-        Motifs.append(Dna[0][i:i+k])
+        Motifs.append(Dna[0][m:m+k])
         for j in range(1, t):
             P = Profile(Motifs[0:j])
             Motifs.append(ProfileMostProbablePattern(Dna[j], k, P))
-            if Score(Motifs) < Score(BestMotifs):
-                BestMotifs = Motifs
+        if Score(Motifs) < Score(BestMotifs):
+            BestMotifs = Motifs
     return BestMotifs
